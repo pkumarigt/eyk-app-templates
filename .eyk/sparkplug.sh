a82912882199838bbaf5767b85d99b9742048f85
Paaras <paaras.kumar@ignitetech.com>
2022-01-19 16:40:38 +0530

    new welcome message

--- .eyk/sparkplug.sh
+++ .eyk/sparkplug.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-
+# !/bin/bash
 # Examples of how to render .yml files from env vars before starting the app itself.
+
 # Render database.yml from env vars
-#erb -T - .eyk/config/database.yml.erb > config/database.yml
+# erb -T - .eyk/config/database.yml.erb > config/database.yml
 
 # Render secrets.yml.erb
-#erb -T - .eyk/config/secrets.yml.erb > config/secrets.yml
+# erb -T - .eyk/config/secrets.yml.erb > config/secrets.yml
 
 # Render sidekiq configuration
-#erb -T - .eyk/config/sidekiq.yml.erb > config/sidekiq.yml
+# erb -T - .eyk/config/sidekiq.yml.erb > config/sidekiq.yml
